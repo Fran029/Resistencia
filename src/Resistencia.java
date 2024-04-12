f1035c609ae277f0d4aa4a8d568e204621dd83fa
Francisca Robles <flanna.jpg@gmail.com>
2024-04-12 19:23:26 -0400

    bucle

--- src/Resistencia.java
+++ src/Resistencia.java
@@ -13,12 +13,24 @@ public class Resistencia {
         // Ingreso de datos por consola
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese el r1");
-         r1 = sc.nextInt();
-        System.out.println("Ingrese el r2");
-        r2 = sc.nextInt();
-        System.out.println("Ingrese el r3");
-        r3 = sc.nextInt();
+        do {
+            System.out.println("Ingrese el r1 (debe ser un valor mayor a cero):");
+            r1 = sc.nextInt();
+        } while (r1 <= 0);
+
+        // Leer y verificar r2
+        do {
+            System.out.println("Ingrese el r2 (debe ser un valor mayor a cero):");
+            r2 = sc.nextInt();
+        } while (r2 <= 0);
+
+        // Leer y verificar r3
+        do {
+            System.out.println("Ingrese el r3 (debe ser un valor mayor a cero):");
+            r3 = sc.nextInt();
+        } while (r3 <= 0);
+
+
 
         rt = 1/(1d/r1 + 1d/r2 + 1d/r3) ;
         DecimalFormat df = new DecimalFormat("#.#");
